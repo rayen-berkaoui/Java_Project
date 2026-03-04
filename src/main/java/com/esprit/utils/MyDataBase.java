f7f0616b51583203605e9c9bdc42331e2dbbe0e5
unknown <dhiaarfaoui987@gmail.com>
2026-03-04 04:25:13 +0100

    Resolve merge conflicts

---
+++ src/main/java/com/esprit/utils/MyDataBase.java
@@ -0,0 +1,35 @@
+package com.esprit.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDataBase {
+
+    private static MyDataBase instance;
+    private Connection connection;
+
+    private final String URL = "jdbc:mysql://localhost:3306/tabaany";
+    private final String USER = "root"; // ton user
+    private final String PASSWORD = ""; // ton mot de passe
+
+    private MyDataBase() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connexion à la base OK !");
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion : " + e.getMessage());
+        }
+    }
+
+    public static MyDataBase getInstance() {
+        if (instance == null) {
+            instance = new MyDataBase();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
